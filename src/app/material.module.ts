c95a537b0f9c4f55b085d10f031ace0f5e240b81
kennedybabu <kennedynjogu8@gmail.com>
2024-02-08 23:30:06 +0300

    feature: add and style the sidebar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,7 +6,9 @@ import {  MatInputModule } from  "@angular/material/input"
 import { MatDatepickerModule } from "@angular/material/datepicker"
 import { MatNativeDateModule } from "@angular/material/core";
 import { MatCheckboxModule } from "@angular/material/checkbox"
-
+import { MatSidenavModule } from "@angular/material/sidenav"
+import { MatToolbarModule } from "@angular/material/toolbar"
+import { MatListModule } from "@angular/material/list"
 
 @NgModule({
     imports: [
@@ -16,7 +18,10 @@ import { MatCheckboxModule } from "@angular/material/checkbox"
         MatInputModule,
         MatDatepickerModule,
         MatNativeDateModule,
-        MatCheckboxModule
+        MatCheckboxModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatListModule
     ],
     exports: [
         MatButtonModule,
@@ -25,7 +30,11 @@ import { MatCheckboxModule } from "@angular/material/checkbox"
         MatInputModule,
         MatDatepickerModule,
         MatNativeDateModule,
-        MatCheckboxModule]
+        MatCheckboxModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatListModule
+    ]
 })
 
 

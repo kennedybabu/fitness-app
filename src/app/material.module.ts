b21f39f9a9af5d1976ab94ed6d9c3fbcb328ae25
kennedybabu <kennedynjogu8@gmail.com>
2024-02-10 07:40:32 +0300

    feature: add tabs on the training component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -9,6 +9,8 @@ import { MatCheckboxModule } from "@angular/material/checkbox"
 import { MatSidenavModule } from "@angular/material/sidenav"
 import { MatToolbarModule } from "@angular/material/toolbar"
 import { MatListModule } from "@angular/material/list"
+import { MatTabsModule } from "@angular/material/tabs"
+
 
 @NgModule({
     imports: [
@@ -21,7 +23,8 @@ import { MatListModule } from "@angular/material/list"
         MatCheckboxModule,
         MatSidenavModule,
         MatToolbarModule,
-        MatListModule
+        MatListModule,
+        MatTabsModule
     ],
     exports: [
         MatButtonModule,
@@ -33,7 +36,8 @@ import { MatListModule } from "@angular/material/list"
         MatCheckboxModule,
         MatSidenavModule,
         MatToolbarModule,
-        MatListModule
+        MatListModule,
+        MatTabsModule
     ]
 })
 

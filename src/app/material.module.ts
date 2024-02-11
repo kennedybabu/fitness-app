64aa325ff5ee14b4c4ee847ad2ed5de07aa403c5
kennedybabu <kennedynjogu8@gmail.com>
2024-02-11 13:37:35 +0300

    feature: add a spinner

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -12,7 +12,7 @@ import { MatListModule } from "@angular/material/list"
 import { MatTabsModule } from "@angular/material/tabs"
 import { MatCardModule } from "@angular/material/card"
 import { MatSelectModule } from "@angular/material/select"
-
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner"
 
 @NgModule({
     imports: [
@@ -28,7 +28,8 @@ import { MatSelectModule } from "@angular/material/select"
         MatListModule,
         MatTabsModule,
         MatCardModule,
-        MatSelectModule
+        MatSelectModule,
+        MatProgressSpinnerModule
     ],
     exports: [
         MatButtonModule,
@@ -43,7 +44,8 @@ import { MatSelectModule } from "@angular/material/select"
         MatListModule,
         MatTabsModule,
         MatCardModule,
-        MatSelectModule
+        MatSelectModule,
+        MatProgressSpinnerModule
     ]
 })
 
--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
   styleUrls: ['./training.component.scss']
 })
-export class TrainingComponent {
+export class TrainingComponent implements OnInit {
+  ongoingTraining = false
+
+  constructor() {}
+
+
+  ngOnInit(): void {
+      
+  }
 
 }

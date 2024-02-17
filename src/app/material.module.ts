3b5e81cc62c031ae2327a53bd6a7c2350c0c3745
kennedybabu <kennedynjogu8@gmail.com>
2024-02-17 16:44:25 +0300

    feature: add table sorting

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -15,6 +15,8 @@ import { MatSelectModule } from "@angular/material/select"
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner"
 import { MatDialogModule } from "@angular/material/dialog"
 import { MatTableModule } from "@angular/material/table"
+import { MatSortModule } from "@angular/material/sort";
+
 
 @NgModule({
     imports: [
@@ -33,7 +35,8 @@ import { MatTableModule } from "@angular/material/table"
         MatSelectModule,
         MatProgressSpinnerModule,
         MatDialogModule,
-        MatTableModule
+        MatTableModule,
+        MatSortModule
     ],
     exports: [
         MatButtonModule,
@@ -51,7 +54,8 @@ import { MatTableModule } from "@angular/material/table"
         MatSelectModule,
         MatProgressSpinnerModule,
         MatDialogModule,
-        MatTableModule
+        MatTableModule,
+        MatSortModule
     ]
 })
 
--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-past-trainings',
   templateUrl: './past-trainings.component.html',
   styleUrls: ['./past-trainings.component.scss']
 })
-export class PastTrainingsComponent implements OnInit {
+export class PastTrainingsComponent implements OnInit,AfterViewInit {
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state']
   dataSource = new MatTableDataSource<Exercise>()
 
+
+  @ViewChild(MatSort) sort!: MatSort
   constructor(private trainingService:TrainingService){}
 
   ngOnInit(): void {
       this.dataSource.data = this.trainingService.getAllExercises()
+  }
+
+
+  ngAfterViewInit(): void {
+      this.dataSource.sort = this.sort
   }
 }

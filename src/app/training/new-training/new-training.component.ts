3892bb0c425458cf1887e97be03e8ec9375bbaa4
kennedybabu <kennedynjogu8@gmail.com>
2024-02-21 13:09:20 +0300

    feature: install angularfire2

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -3,7 +3,12 @@ import { Exercise } from './../exercise.model';
 import { Component, OnInit, inject} from '@angular/core';
 import { TrainingService } from '../training.service';
 import { NgForm } from '@angular/forms';
-import { Firestore, collection, collectionData, getDocs, query } from "@angular/fire/firestore"
+import { Firestore, collectionData, getDocs, onSnapshot, query, collection } from "@angular/fire/firestore"
+// import { doc } from "fire"
+import { doc } from '@angular/fire/firestore';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+
 
 @Component({
   selector: 'app-new-training',
@@ -13,14 +18,18 @@ import { Firestore, collection, collectionData, getDocs, query } from "@angular/
 export class NewTrainingComponent implements OnInit {
   
   exercises$!: Observable<Exercise[]>
-  firestore: Firestore = inject(Firestore)
   list$ = collectionData(collection(this.db, 'availableExercises')) as Observable<Exercise[]>
 
   constructor(private trainingService: TrainingService,
     private db: Firestore, 
+    private fbDb: AngularFireDatabase
     ){
-      const itemCollection = collection(this.firestore, 'vailableExercises')
-      const any = collectionData(itemCollection)      
+      // const itemCollection = collection(this.db, 'vailableExercises')
+      // const any = collectionData(itemCollection).subscribe((res) => {
+      //   console.log(res)
+      // })   
+      
+      // console.log(any)
     }
 
   onStartTraining(form: NgForm) {
@@ -29,7 +38,26 @@ export class NewTrainingComponent implements OnInit {
 
 
   ngOnInit(): void {
-    
+    // this.db.collection('a')
+    // const unsub = onSnapshot(doc(this.db, 'availableExercises'), (doc) => {
+    //   console.log(doc)
+    // })
+
+    // console.log(unsub)
   }
+
+
+  // getExercises() {
+  //   const all = this.fbDb.collection('availableExercises').snapshotChanges().map(docArray => {
+  //     return docArray.map(doc => {
+  //       return {
+  //         id: doc.payload.doc.id,
+  //         ...doc.payload.doc.data()
+  //       }
+  //     })
+  //   }).subscribe(result => {
+  //     console.log(result)
+  //   })
+  // }
   
 }
